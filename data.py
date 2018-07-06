adf710f31c5bc05ee6458fc3015a7804482b03cd
Domenico Tortorella <dom.tortorella@gmail.com>
2018-07-06 11:10:18 +0200

    Fixed source and target loading

--- data.py
+++ data.py
@@ -10,38 +10,34 @@ def load_chars(lang):
     return util.map_characters_to_integers(chars)
 
 def load_source(file_path, num_lines, max_line_length, chars):
-    source = np.empty([num_lines, max_line_length+1], dtype=np.int32)
+    source = np.empty([num_lines, max_line_length], dtype=np.int16)
     with open(file_path, 'r', encoding='utf-8') as f:
-        line_index = 0
-        while True:
+        for line_index in range(0, num_lines):
             line = f.readline()
             if not line:
                 break
             line_len = len(line)
-            line = util.encode_for_embedding(line,chars)
+            line = util.encode_for_embedding(line, chars)
             for i in range(0, line_len):
                 source[line_index, i] = line[i]
-            for i in range(line_len, max_line_length + 1):
+            for i in range(line_len, max_line_length):
                 source[line_index, i] = chars[END_SEQUENCE]
-            line_index += 1
     return source
 
 def load_target(file_path, num_lines, max_line_length, chars):
-    target = np.empty([num_lines, max_line_length+2], dtype=np.int32)
+    target = np.empty([num_lines, max_line_length+1], dtype=np.int16)
     with open(file_path, 'r', encoding='utf-8') as f:
-        line_index = 0
-        while True:
+        for line_index in range(0, num_lines):
             line = f.readline()
             if not line:
                 break
             line_len = len(line)
-            line = util.encode_for_embedding(line,chars)
+            line = util.encode_for_embedding(line, chars)
             target[line_index, 0] = chars[START_SEQUENCE]
             for i in range(0, line_len):
                 target[line_index, i+1] = line[i]
-            for i in range(line_len + 1, max_line_length + 2):
+            for i in range(line_len+1, max_line_length+1):
                 target[line_index, i] = chars[END_SEQUENCE]
-            line_index += 1
     return target
 
 def target_to_categorical(target, num_chars):
